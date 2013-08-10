f7bca1a4226313093179aa13dd08ce908f0ec0d6
Ben Hutchison <ben@bluejeans.com>
2013-08-09 22:08:22 -0700

    Test page for photo uploads

--- routes/people.js
+++ routes/people.js
@@ -1,7 +1,10 @@
 var _                = require('lodash');
+var path             = require('path');
 var personRepository = require('../lib/personRepository');
 var photoManager     = require('../lib/photoManager');
 var Q                = require('q');
+var url              = require('url');
+var verror           = require('verror');
 
 var FIELD_WHITELIST = ['fullname', 'desk', 'office', 'email', 'title', 'tags', 'linkedInId', 'mobilePhone', 'workPhone'];
 
@@ -49,15 +52,53 @@ exports.delete = function(req, res, next){
 
 exports.setPhoto = function(req, res, next){
 	var uploadedFile = req.files.photo;
-	var tempPath = uploadedFile.path;
+	var tempPath     = uploadedFile.path;
+	var personId     = req.params.id;
 
-	personRepository.findOne(req.params.id)
+	personRepository.findOne(personId)
 		.then(function(person){
-			return photoManager.importPhoto(tempPath, person._id + '.jpg')
-			// console.log("set %s's photo to %s", person.fullname, uploadedFile.name);
+			return photoManager.importPhoto(tempPath, personId + '.jpg')
 		})
-		.then(function(){
-			res.redirect(303, 'images/photos/'+req.params.id+'.jpg');
+		.then(function(imgInfo){
+			var basename = path.basename(imgInfo.path);
+
+			//TODO this probably won't work through a reverse proxy if the request headers and response paths start getting weird
+			//probably need a configuration option for the user-facing URL mount point of this application
+			var imageUrl = url.format({
+				protocol : req.protocol,
+				host     : req.get('host'),
+				pathname : '/images/photos/' + basename
+			});
+
+			var payload = { files: [{
+				name         : basename,
+				url          : imageUrl,
+				thumbnailUrl : imageUrl,
+				size         : imgInfo.size
+			}]};
+
+			/**
+			 * Browsers that upload using iframes require text/html or text/plain,
+			 * because application/json will create a download dialog.
+			 * 
+			 * @see https://github.com/blueimp/jQuery-File-Upload/wiki/Setup#content-type-negotiation
+			*/
+			res.format({
+				json: function(){
+					res.send(payload);
+				},
+				default: function(){
+					res.send(JSON.stringify(payload));
+				}
+			});
+		})
+		.fail(function(err){
+			res.type('text');
+			res.send(400, err.message);
 		})
 		.fail(next);
+};
+
+exports.uploadTest = function(req, res){
+	res.render('upload-test');
 };
