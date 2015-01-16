ca28be516623fa72872f58f4837ee232148e7edc
Ben Hutchison <ben@bluejeans.com>
2015-01-15 17:36:45 -0800

    Remote Workers link has a globe icon instead of an ellipsis.

--- public/scripts/ListPane.js
+++ public/scripts/ListPane.js
@@ -329,7 +329,7 @@ this.ListPane = (function(){
 					$('<a>', { href: 'sf', title: 'view the San Francisco office', text: 'sf' }),
 					$('<a>', { href: 'ln', title: 'view the London office', text: 'ln' }),
 					$('<a>', { href: 'blr', title: 'view the Bangalore office', text: 'blr' }),
-					$('<a>', { href: 'remote', title: 'view remote workers', text: '\u22EF' })
+					$('<a>', { href: 'remote', title: 'view remote workers', text: '\u014C' })
 				);
 			}
 
